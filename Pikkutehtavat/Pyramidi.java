b50fc416d3c330bebf9d5fecb762f4c74bb521b8
Mikram1 <46792937+Mikram1@users.noreply.github.com>
2020-06-03 18:58:01 +0300

    Rename Pyramidi.java to Pikkutehtavat/Pyramidi.java

--- Pikkutehtavat/Pyramidi.java
+++ Pikkutehtavat/Pyramidi.java
@@ -1,26 +1,26 @@
 package kata;
 
 /**
- * Teht‰v‰nanto: https://www.codewars.com/kata/5797d1a9c38ec2de1f00017b
- * Tekee annetusta merkkijonosta pyramidin (jokaiselle merkille oma kerros) ja n‰ytt‰‰ sen sivulta & p‰‰lt‰ sek‰
- * ilmoittaa k‰ytettyjen "palikoiden" kokonaism‰‰r‰n ja n‰ht‰v‰n m‰‰r‰n.
- * 6 KYU!! Sain t‰ll‰ rankin :)
+ * Teht√§v√§nanto: https://www.codewars.com/kata/5797d1a9c38ec2de1f00017b
+ * Tekee annetusta merkkijonosta pyramidin (jokaiselle merkille oma kerros) ja n√§ytt√§√§ sen sivulta & p√§√§lt√§ sek√§
+ * ilmoittaa k√§ytettyjen "palikoiden" kokonaism√§√§r√§n ja n√§ht√§v√§n m√§√§r√§n.
+ * 6 KYU!! Sain t√§ll√§ rankin :)
  * @author Maikki
  * @version 12.4.2017
  */
 public class Pyramidi {
 
 	/**
-	 * P‰‰ohjelma
-	 * @param args ei k‰ytˆss‰
+	 * P√§√§ohjelma
+	 * @param args ei k√§yt√∂ss√§
 	 */
 	public static void main(String[] args) {
 		System.out.println(watchPyramidFromTheSide("abcde"));
 		System.out.println();
 		System.out.println(watchPyramidFromAbove("abcde"));
 		System.out.println();
-		System.out.println("N‰kyv‰t palikat: " + countVisibleCharactersOfThePyramid("abcde"));
-		System.out.println("Palikoita yhteens‰: " + countAllCharactersOfThePyramid("abcde"));
+		System.out.println("N√§kyv√§t palikat: " + countVisibleCharactersOfThePyramid("abcde"));
+		System.out.println("Palikoita yhteens√§: " + countAllCharactersOfThePyramid("abcde"));
 		System.out.println();
 		System.out.println(watchPyramidFromTheSide("*#"));
 		System.out.println(watchPyramidFromTheSide(null));
@@ -29,11 +29,11 @@ public class Pyramidi {
 	}
 
 	/**
-	 * N‰ytt‰‰ pyramidin sivulta. 
+	 * N√§ytt√§√§ pyramidin sivulta. 
 	 * Esim. "abc" =>     c
 	 * 					 bbb
 	 *                  aaaaa
-	 * @param characters merkkijono, josta tehd‰‰n pyramidi
+	 * @param characters merkkijono, josta tehd√§√§n pyramidi
 	 * @return pyramidi sivulta
 	 */
 	public static String watchPyramidFromTheSide(String characters) {
@@ -53,13 +53,13 @@ public class Pyramidi {
 	  }
 
 	/**
-	 * N‰ytt‰‰ pyramidin ylh‰‰lt‰p‰in.
+	 * N√§ytt√§√§ pyramidin ylh√§√§lt√§p√§in.
 	 * Esim. abc =>    aaaaa
 	 * 				   abbba
 	 * 				   abcba
 	 * 				   abbba
 	 * 				   aaaaa
-	 * @param characters merkkijono, josta tehd‰‰n pyramidi
+	 * @param characters merkkijono, josta tehd√§√§n pyramidi
 	 * @return pyramidi sivulta
 	 */
 	 public static String watchPyramidFromAbove(String characters) {
@@ -86,9 +86,9 @@ public class Pyramidi {
 	  }
 	  
 	 /**
-	  * Palauttaa pyramidin n‰kyv‰t palikat. (Ylh‰‰lt‰p‰in. Suorakulmio => leveys*2)
-	  * @param characters merkkijono, josta tehd‰‰n pyramidi
-	  * @return n‰kyvien palikoiden lukum‰‰r‰
+	  * Palauttaa pyramidin n√§kyv√§t palikat. (Ylh√§√§lt√§p√§in. Suorakulmio => leveys*2)
+	  * @param characters merkkijono, josta tehd√§√§n pyramidi
+	  * @return n√§kyvien palikoiden lukum√§√§r√§
 	  */
 	  public static int countVisibleCharactersOfThePyramid(String characters) {
 		  if(characters == null || characters.isEmpty()) return -1;
@@ -97,9 +97,9 @@ public class Pyramidi {
 	  }
 
 	  /**
-	   * Palauttaa kaikki pyramidissa k‰ytettyjen palikoiden lukum‰‰r‰n.
-	   * @param characters merkkijono, josta tehd‰‰n pyramidi
-	   * @return palikoiden lukum‰‰r‰
+	   * Palauttaa kaikki pyramidissa k√§ytettyjen palikoiden lukum√§√§r√§n.
+	   * @param characters merkkijono, josta tehd√§√§n pyramidi
+	   * @return palikoiden lukum√§√§r√§
 	   */
 	  public static int countAllCharactersOfThePyramid(String characters) {
 		if(characters == null || characters.isEmpty()) return -1;
